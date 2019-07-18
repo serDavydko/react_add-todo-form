06c22c81b7169068a15e0a91a7a3d980e2ee736b
Serhii <davydko.serhiy@gmail.com>
2019-07-19 02:34:37 +0300

    Updates

---
+++ precache-manifest.07d7d60370a60d75bd4503d9c334e8fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88a4a27cd1af6cf360a339124e24e25",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "5d8d498c682b17a41132",
+    "url": "/react_add-todo-form/static/css/main.5f04d6ce.chunk.css"
+  },
+  {
+    "revision": "ae3ef7590a882c4465b7",
+    "url": "/react_add-todo-form/static/js/2.2f1b5c2a.chunk.js"
+  },
+  {
+    "revision": "5d8d498c682b17a41132",
+    "url": "/react_add-todo-form/static/js/main.b50ed182.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
